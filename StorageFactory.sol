42f9430a2c5cc71f02f444da932c297fffaa36ec
Márton Borsos <borsos.marton95@gmail.com>
2022-01-06 13:49:25 +0100

    added function to getStorage

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -2,32 +2,28 @@
 
 pragma solidity ^0.6.0;
 
-import "./SimpleStorage.sol"; 
+import "./SimpleStorage.sol";
 
 contract StorageFactory is SimpleStorage {
-    
+
     SimpleStorage[] public simpleStorageArray;
-    
+
     function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
-    
-    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
-        // Address 
-        // ABI 
-        //this line has an explicit cast to the address type and initializes a new SimpleStorage object from the address
-        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber); 
 
-        //this line simply gets the SimpleStorage object at the index _simpleStorageIndex in the array simpleStorageArray
-        //simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    function getStorage(uint256 _simpleStorageIndex) private view returns (SimpleStorage) {
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
     }
-    
-    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        //this line has an explicit cast to the address type and initializes a new SimpleStorage object from the address 
-        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve(); 
 
-        //this line simply gets the SimpleStorage object at the index _simpleStorageIndex in the array simpleStorageArray
-        //return simpleStorageArray[_simpleStorageIndex].retrieve(); 
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address
+        // ABI Application Binary Interface
+        getStorage(_simpleStorageIndex).store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return getStorage(_simpleStorageIndex).retrieve();
     }
 }
